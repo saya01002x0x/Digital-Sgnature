53cabec071aed0a6cdeec444bf894565d6c128cf
Nguyễn Chiêu Văn <chieuvanbn@gmail.com>
2025-09-23 15:46:19 +0700

    <create>[vannc]: create spring web

---
+++ digital-signature/src/main/java/sis/hust/edu/vn/digital_signature/DigitalSignatureWebApplication.java
@@ -0,0 +1,13 @@
+package sis.hust.edu.vn.digital_signature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalSignatureWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalSignatureWebApplication.class, args);
+	}
+
+}
---
+++ digital-signature/src/main/java/sis/hust/edu/vn/digital_signature/ServletInitializer.java
@@ -0,0 +1,13 @@
+package sis.hust.edu.vn.digital_signature;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DigitalSignatureWebApplication.class);
+	}
+
+}
